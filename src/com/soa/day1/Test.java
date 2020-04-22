50b9cd9cd612dbc8b0b834b71c3e07be5538354e
lml <soalin228@163.com>
2020-04-22 21:28:36 +0800

    feat: 每日一题 day1

---
+++ src/com/soa/day1/Test.java
@@ -0,0 +1,9 @@
+package com.soa.day1;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution leetCode = new Solution();
+        boolean palindromeType = leetCode.isPalindrome("a1,xa");
+        System.out.println(palindromeType);
+    }
+}
